a877c20eabbf0c8f777131e2282ba9b64bb69adb
Mageswarikaruppasamy <mageswari31k@gmail.com>
2025-01-24 22:17:51 +0530

    Few more examples added

---
+++ Lecture1/typeconversion.py
@@ -0,0 +1,13 @@
+#Type of the value
+print(type(5))
+print(type(5.0))
+print(type("5"))
+print(type(True))
+
+#Type conversion
+print(float(5))
+print(int(5.0))
+print(str(5))
+print(bool(0))
+print(bool(1))
+print(round(5.5))
